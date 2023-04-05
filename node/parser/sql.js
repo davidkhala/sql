b6372d2984cda18e605c82be1a2d0ec83c895616
David Liu <david.yx.liu@oracle.com>
2023-04-06 01:31:56 +0800

    [WIP] dt-sql-parser

--- node/parser/sql.js
+++ node/parser/sql.js
@@ -1,11 +1,48 @@
-import { GenericSQL } from 'dt-sql-parser';
+import {GenericSQL, SqlParserVisitor} from 'dt-sql-parser';
 import * as assert from "assert";
 
 const parser = new GenericSQL();
 
-export function parse(sql){
+export function validate(sql) {
     const errors = parser.validate(sql);
     assert.equal(errors.length, 0)
 }
+
+export function parse(sql, pretty) {
+    const tokens = parser.getAllTokens(sql)
+    if (pretty) {
+        return tokens.map(({start, stop}) => sql.substring(start, stop + 1))
+    }
+    return tokens
+
+}
+
+const defaultVisitor = (data, context) => {
+    console.info(data)
+}
+
+export function traverse(sql, onTable = defaultVisitor, onSelect = defaultVisitor) {
+
+    class AbstractVisitor extends SqlParserVisitor {
+        // overwrite visitTableName
+        visitTableName(context) {
+            const tableName = context.getText()
+            onTable(tableName, context)
+        }
+
+        // overwrite visitSelectElements
+        visitSelectElements(context) {
+            const selectElements = context.getText()
+            onSelect(selectElements, context)
+        }
+    }
+
+    const tree = parser.parse(sql)
+    const visitor = new AbstractVisitor()
+    visitor.visit(tree)
+
+    return visitor
+}
+
 
 
--- node/parser/test/sql.js
+++ node/parser/test/sql.js
@@ -1,7 +1,14 @@
-import {parse} from '../sql.js'
+import {parse, validate, traverse} from '../sql.js'
 describe('sql parser', function () {
-    it('', ()=>{
-        const correctSql = 'select id,name from user1;';
-        parse(correctSql)
+    const correctSql = 'SELECT id,name from user1;';
+    it('', () => {
+        validate(correctSql)
+    })
+    it('tokenize', () => {
+        const tokens = parse(correctSql, true)
+        console.info(tokens)
+    })
+    it('traverse', ()=>{
+        traverse(correctSql)
     })
 });
