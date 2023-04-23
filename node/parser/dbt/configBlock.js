d7d1a86d667c0af9af1f9f8c321f65c65a99a4fe
David Liu <david.yx.liu@oracle.com>
2023-04-23 23:08:52 +0800

    add incremental model

--- node/parser/dbt/configBlock.js
+++ node/parser/dbt/configBlock.js
@@ -8,3 +8,21 @@ export function build(configs = {}) {
 }}`
 }
 
+/**
+ * https://docs.getdbt.com/docs/build/incremental-models
+ * @param unique_key
+ * @param exclude_columns
+ * @returns {string}
+ */
+export function incrementalModel(unique_key,exclude_columns = [] ) {
+    const configs = {
+        materialized: 'incremental',
+        unique_key,
+        on_schema_change: 'sync_all_columns',
+        incremental_strategy: 'merge',
+        merge_exclude_columns: exclude_columns
+    }
+    return build(configs)
+}
+
+
